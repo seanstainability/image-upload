1435dff0cfb72028359fbbe986e42935338671ac
seanstainability <seanstainability@gmail.com>
2021-09-09 14:16:46 +0900

    Add: 좋아요 기능

--- server/models/Image.js
+++ server/models/Image.js
@@ -3,10 +3,17 @@ const mongoose = require("mongoose");
 const ImageSchema = new mongoose.Schema(
   {
     user: {
-      _id: { type: mongoose.Types.ObjectId, require: true, index: true },
-      nickname: { type: String, require: true },
-      email: { type: String, require: true },
+      _id: { type: mongoose.Types.ObjectId, required: true, index: true },
+      nickname: { type: String, required: true },
+      email: { type: String, required: true },
     },
+    likes: [
+      {
+        _id: { type: mongoose.Types.ObjectId, required: true },
+        nickname: { type: String, required: true },
+        email: { type: String, required: true },
+      },
+    ],
     public: { type: Boolean, required: true, default: false },
     key: { type: String, required: true },
     originalFileName: { type: String, required: true },
--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -50,6 +50,54 @@ imageRouter.delete("/:imageId", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
-imageRouter.patch("/:imageId/like", (req, res) => {});
+imageRouter.patch("/:imageId/like", async (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    console.log(req.params);
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 이미지 형식입니다.");
+    const image = await Image.findOneAndUpdate(
+      { _id: req.params.imageId },
+      {
+        $addToSet: {
+          // 동일한 게 없을 경우에만 업데이트
+          likes: {
+            _id: req.user.id,
+            nickname: req.user.nickname,
+            email: req.user.email,
+          },
+        },
+      },
+      { new: true }
+    );
+    res.json(image);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+imageRouter.patch("/:imageId/unlike", async (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    console.log(req.params);
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 이미지 형식입니다.");
+    const image = await Image.findOneAndUpdate(
+      { _id: req.params.imageId },
+      {
+        $pull: {
+          likes: {
+            _id: req.user.id,
+          },
+        },
+      },
+      { new: true }
+    );
+    res.json(image);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+});
 
 module.exports = { imageRouter };
